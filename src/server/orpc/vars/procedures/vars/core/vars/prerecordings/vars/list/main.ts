04bdadb0c767825a8b330ce18f8fb8cb6e706e10
Sebastian Pietras <me@spietras.dev>
2026-05-03 01:01:08 +0200

    Updated OpenAPI specifications and added content type validation (#18)

--- src/server/orpc/vars/procedures/vars/core/vars/prerecordings/vars/list/main.ts
+++ src/server/orpc/vars/procedures/vars/core/vars/prerecordings/vars/list/main.ts
@@ -8,22 +8,16 @@ export const list = orpcServerRootBase.core.prerecordings.list.handler(
   async ({ errors, input }) => {
     const { event, ...query } = input;
 
-    const {
-      data: prerecordingsEventListData,
-      response: prerecordingsEventListResponse,
-    } = await state.current.apis.numbat.prerecordingsEventList({
-      path: { event: event },
-      query: mapValues(query ?? {}, (value) =>
-        isJSONValue(value) ? JSON.stringify(value) : value,
-      ),
-    });
+    const { data: prerecordingsEventListData } =
+      await state.current.apis.numbat.prerecordingsEventList({
+        path: { event: event },
+        query: mapValues(query ?? {}, (value) =>
+          isJSONValue(value) ? JSON.stringify(value) : value,
+        ),
+      });
 
-    if (prerecordingsEventListData === undefined) {
-      if (prerecordingsEventListResponse.status === 404)
-        throw errors.NOT_FOUND();
-
+    if (prerecordingsEventListData === undefined)
       throw errors.INTERNAL_SERVER_ERROR();
-    }
 
     return prerecordingsEventListData;
   },
